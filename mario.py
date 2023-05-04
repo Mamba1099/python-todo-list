4954bb3859ac9d2f6b465b293a13618f3bc2b094
Mamba1099 <mambakaranja240@gmail.com>
2023-05-04 17:02:32 +0300

    creating blocks

---
+++ mario.py
@@ -0,0 +1,13 @@
+def main():
+   print_square(3)
+ 
+def print_square(size):
+     for i in range(size):
+         
+        #for each brick in row
+        for j in range(size):
+           print("#", end="")    
+        print()
+
+    
+main()        
